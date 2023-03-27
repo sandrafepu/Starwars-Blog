5c136dff25869d1cc4278cf7c9b03dd04f164770
sandrafepu <sandrafepu@gmail.com>
2023-03-27 18:59:29 +0000

    Respuesta exitosa del fetch character

--- src/js/component/CardsCharacters.js
+++ src/js/component/CardsCharacters.js
@@ -5,27 +5,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Link } from "react-router-dom";
 
-export const CardCharacter = () => {
+export const CardCharacter = (props) => {
     return (
-
+          
             <Card style={{ width: '18rem' }}>
                <Card.Img variant="top" src="https://via.placeholder.com/400x200" />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
+              <h2 id="person-title">Person Title</h2>
               </Card.Body>
               <ListGroup className="list-group-flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+              <p id="person-gender"> Gender: </p>
+              <p id="person-eye_color"> Eye_color:</p>
+              <p id="person-hair_color"> Hair_color:</p>
               </ListGroup>
               <Card.Body>
                 < Link to="/character"><Button variant="outline-primary">Learn More!</Button></Link> 
                 <Button variant="outline-warning float-end"><FontAwesomeIcon icon={faHeart}/></Button>
               </Card.Body>
             </Card>
+                    
     );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../../styles/home.css";
 import { CardCharacter } from "../component/CardsCharacters";
 import { CardPlanet } from "../component/CardsPlanets";
 
-export const Home = () => (
+export const Home = () =>{ 
+
+	const [character, setCharacter] = useState ([]);
+
+	useEffect(() => {
+		fetch("https://swapi.dev/api/people/7/")
+		.then((response) => response.json())
+		.then((data) => {
+			const card = document.querySelector('.card');
+			const title = card.querySelector('#person-title');
+			const gender = card.querySelector('#person-gender');
+			const eye_color = card.querySelector('#person-eye_color');
+			const hair_color = card.querySelector('#person-hair_color');
+			title.textContent = data.name;
+			gender.textContent = data.gender;
+			eye_color.textContent = data.eye_color;
+			hair_color.textContent = data.hair_color;
+			console.log(data);
+			setCharacter(data.name);
+			});
+	}, []);
+
+
+	return(
 	<div className="mt-5">
         <h1><strong>Characters</strong></h1>
 		<CardCharacter></CardCharacter>
@@ -14,3 +37,4 @@ export const Home = () => (
 	</div>
 	</div>
 );
+};
